4bd2093ed1d6b4fd21d207371c25e5cda2b95ede
Pedro Lucas Dos Santos Ribeiro <pedrol.plsr@gmail.com>
2022-11-29 09:18:21 -0300

    update

--- app.js
+++ app.js
@@ -22,10 +22,17 @@ app.use(express.json());
 //definindo as rotas
 const router = express.Router();
 
+<<<<<<< Updated upstream
 router.get("/", (req, res) => res.json({ message: "Connected to Betavo DB!" }));
 
 // ==== CARDS API ====
 // GET /cards || GET /cards/{id}
+=======
+router.get("/", (req, res) => res.json({ message: "Funcionando!" }));
+
+// ==== CARDS COLLECITON ====
+// GET
+>>>>>>> Stashed changes
 router.get("/cards/:id?", async function (req, res, next) {
   try {
     const db = await connect();
@@ -44,7 +51,11 @@ router.get("/cards/:id?", async function (req, res, next) {
   }
 });
 
+<<<<<<< Updated upstream
 // POST /cards
+=======
+// POST
+>>>>>>> Stashed changes
 router.post("/cards", async function (req, res, next) {
   try {
     const customer = req.body;
@@ -56,7 +67,11 @@ router.post("/cards", async function (req, res, next) {
   }
 });
 
+<<<<<<< Updated upstream
 // PUT /cards/{id}
+=======
+// PUT
+>>>>>>> Stashed changes
 router.put("/cards/:id", async function (req, res, next) {
   try {
     const customer = req.body;
@@ -72,7 +87,11 @@ router.put("/cards/:id", async function (req, res, next) {
   }
 });
 
+<<<<<<< Updated upstream
 // DELETE /cards/{id}
+=======
+// DELETE
+>>>>>>> Stashed changes
 router.delete("/cards/:id", async function (req, res, next) {
   try {
     const db = await connect();
@@ -86,6 +105,70 @@ router.delete("/cards/:id", async function (req, res, next) {
     res.status(400).json({ erro: `${ex}` });
   }
 });
+<<<<<<< Updated upstream
+=======
+
+// ==== BETS COLLECITON ====
+// GET
+router.get("/bets/:id?", async function (req, res, next) {
+  try {
+    const db = await connect();
+    if (req.params.id) {
+      const match = await db.collection("matches").findOne({
+        _id: new ObjectId(req.params.id),
+      });
+    } else {
+      res.json(await db.collection("bets").find().toArray());
+    }
+  } catch (ex) {
+    console.log(ex);
+    res.status(400).json({ erro: `${ex}` });
+  }
+});
+
+// POST
+router.post("/bets", async function (req, res, next) {
+  try {
+    const customer = req.body;
+    const db = await connect();
+    res.json(await db.collection("bets").insertOne(customer));
+  } catch (ex) {
+    console.log(ex);
+    res.status(400).json({ erro: `${ex}` });
+  }
+});
+
+// PUT
+router.put("/bets/:id", async function (req, res, next) {
+  try {
+    const customer = req.body;
+    const db = await connect();
+    res.json(
+      await db
+        .collection("bets")
+        .updateOne({ _id: new ObjectId(req.params.id) }, { $set: customer })
+    );
+  } catch (ex) {
+    console.log(ex);
+    res.status(400).json({ erro: `${ex}` });
+  }
+});
+
+// DELETE
+router.delete("/bets/:id", async function (req, res, next) {
+  try {
+    const db = await connect();
+    res.json(
+      await db
+        .collection("bets")
+        .deleteOne({ _id: new ObjectId(req.params.id) })
+    );
+  } catch (ex) {
+    console.log(ex);
+    res.status(400).json({ erro: `${ex}` });
+  }
+});
+>>>>>>> Stashed changes
 
 app.use("/", router);
 
@@ -94,3 +177,7 @@ console.log(
   `Server runnning on PORT ${PORT}\nServer URL: http://localhost:3000/`
 );
 app.listen(PORT);
+<<<<<<< Updated upstream
+=======
+console.log(`Server runnning on PORT ${PORT}`);
+>>>>>>> Stashed changes
